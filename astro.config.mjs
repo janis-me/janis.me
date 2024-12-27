0a4d3fe29610836867a77a51c847f7c4380349db
Janis Jansen <oss@janis.me>
2024-12-27 21:07:30 +0100

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 
+import webmanifest from "astro-webmanifest";
+
 import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -12,7 +14,24 @@ const __dirname = dirname(__filename);
 // https://astro.build/config
 export default defineConfig({
   site: "https://janis.me",
-  integrations: [solidJs(), sitemap()],
+  integrations: [
+    solidJs(),
+    sitemap(),
+    webmanifest({
+      name: "janis.me - my personal website",
+
+      /**
+       * optional
+       **/
+      icon: "src/assets/astronaut.svg", // source for favicon & icons
+      short_name: "janis.me",
+      description: "Janis Jansen - Fullstack dev & creator",
+      start_url: "/",
+      theme_color: "#212529",
+      background_color: "#212529",
+      display: "standalone",
+    }),
+  ],
   vite: {
     resolve: {
       alias: {
