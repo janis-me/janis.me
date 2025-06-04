68835ba23dd0363ed4dab0e9285027e0d0faf53f
Janis Jansen <oss@janis.me>
2025-06-04 09:01:59 +0200

    feat: improve chunk file naming with extension

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'node:url';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { transformerMetaHighlight } from '@shikijs/transformers';
+import genericBuildFilenames from 'astro-generic-build-filenames';
 import webmanifest from 'astro-webmanifest';
 import { defineConfig, passthroughImageService } from 'astro/config';
 import autolinkHeadings from 'rehype-autolink-headings';
@@ -46,6 +47,7 @@ export default defineConfig({
         ],
       ],
     }),
+    genericBuildFilenames(),
   ],
   markdown: {
     shikiConfig: {
